d1c186accc31bf03ddaa4fc192b425a81f8ae905
Eric Bowser <ericryanbowser@gmail.com>
2025-05-27 21:44:06 -0600

    ..

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import {createRoot} from 'react-dom/client';
 import App from './App.js';
 import {BrowserRouter} from "react-router-dom";
+import './styles/input.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
